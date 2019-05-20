be80195ef1fe1d4211c263e9d4611aa04ac25e29
depdep <depdep>
2019-05-20 16:25:13 +0900

    all delete

--- main.js
+++ main.js
@@ -1,21 +1,46 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
+ 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    console.log(queryData);
-    if(_url == '/'){
-        _url = '/index.html';
+    var pathname = url.parse(_url, true).pathname;
+    var title = queryData.id;  
+
+    if(pathname === '/') {
+      
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, data){
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${data}</p>
+        </body>
+        </html>
+        `;
+
+        response.writeHead(200);
+        response.end(template);
+
+      });
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
     }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + _url));
+
+
  
 });
 app.listen(3000);
